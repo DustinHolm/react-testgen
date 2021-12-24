a31e6b0a1e4356b58f8fb4c99afc44bb7061cd94
dustin <dustin.holm@gmx.net>
2021-12-24 22:22:58 +0100

    adjusted tests

--- src/ScaffoldBuilder.test.ts
+++
@@ -1,9 +0,0 @@
-import ScaffoldBuilder from "./ScaffoldBuilder"
-
-describe("ScaffoldBuilder Tests", () => {
-    test("Matches snapshot", () => {
-        const builder = new ScaffoldBuilder("Test.tsx")
-        const returnedString = builder.build()
-        expect(returnedString).toMatchSnapshot()
-    })
-})
--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,5 @@
-import { createModulePath } from "./utils"
+import { ClassElement, Export, ExportType, FunctionElement } from "./types"
+import { createModulePath, getPropsForExport } from "./utils"
 
 describe("utils Tests", () => {
     describe("createModulePath Tests", () => {
@@ -21,6 +22,48 @@ describe("utils Tests", () => {
             const directory = "/home/admin/projects/webapp"
             const returnedString = createModulePath(directory, file)
             expect(returnedString).toEqual("/home/admin/projects/other_webapp/Test.ts")
+        })
+    })
+
+    describe("getPropsForExport Tests", () => {
+        const givenAttribute = { name: "test", type: "test" }
+        const expectedAttributes = [givenAttribute, givenAttribute]
+
+        test("For Function", () => {
+            const givenAttributes = [givenAttribute, givenAttribute]
+            const element: FunctionElement = {
+                returnsJSX: false,
+                type: ExportType.Function,
+                returnType: "number",
+                parameters: givenAttributes
+            }
+            const exportElement: Export = { name: "Test", isDefault: false, element: element }
+            const returnedAttributes = getPropsForExport(exportElement)
+            expect(returnedAttributes).toEqual(expectedAttributes)
+        })
+
+        test("For Class", () => {
+            const element: ClassElement = {
+                returnsJSX: false,
+                type: ExportType.Class,
+                methods: [],
+                constructors: [{ parameters: [givenAttribute, givenAttribute] }]
+            }
+            const exportElement: Export = { name: "Test", isDefault: false, element: element }
+            const returnedAttributes = getPropsForExport(exportElement)
+            expect(returnedAttributes).toEqual(expectedAttributes)
+        })
+
+        test("For Class with separate constructors", () => {
+            const element: ClassElement = {
+                returnsJSX: false,
+                type: ExportType.Class,
+                methods: [],
+                constructors: [{ parameters: [givenAttribute] }, { parameters: [givenAttribute] }]
+            }
+            const exportElement: Export = { name: "Test", isDefault: false, element: element }
+            const returnedAttributes = getPropsForExport(exportElement)
+            expect(returnedAttributes).toEqual(expectedAttributes)
         })
     })
 })
